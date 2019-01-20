57a5f586d343a6095cb12aafc5a07de2e0815292
Oscar <oscar_e_c@yahoo.com>
2019-01-20 09:33:57 -0800

    map

---
+++ Assets/Scripts/LoadNext.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadNext : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("MyLoadingFunction", 5f);
+    }
+    void MyLoadingFunction()
+    {
+        Application.LoadLevel("Main");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
