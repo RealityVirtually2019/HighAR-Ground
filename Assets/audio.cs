ea8aae11e699dd74dab2f8a0e0185f4eedf57c19
Oscar <oscar_e_c@yahoo.com>
2019-01-20 09:45:10 -0800

    added a sound holder prefab

---
+++ Assets/audio.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audio : MonoBehaviour {
+    public AudioClip soundFile;
+    AudioSource mySound;
+    private void Awake()
+    {
+       
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Invoke("MysoundFunction", 3f);
+    }
+
+    void MysoundFunction()
+    {
+        mySound.PlayOneShot(soundFile);
+    }
+}
